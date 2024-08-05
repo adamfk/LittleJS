87f7185ac6916268f53248380a1f6d3aecef1706
Adam Fraser-Kruck <adamfk@gmail.com>
2024-08-05 08:33:28 -0500

    move enemy to new file

--- examples/platformer/gameObjects.js
+++ examples/platformer/gameObjects.js
@@ -123,64 +123,6 @@ class Coin extends EngineObject
 
 ///////////////////////////////////////////////////////////////////////////////
 
-class Enemy extends GameObject 
-{
-    constructor(pos) 
-    { 
-        super(pos, vec2(.9,.9), spriteAtlas['enemy']);
-
-        this.drawSize = vec2(1);
-        this.color = hsl(rand(), 1, .7);
-        this.health = 5;
-        this.bounceTime = new Timer(rand(1e3));
-        this.setCollision(true, false);
-    }
-
-    update()
-    {
-        super.update();
-        
-        if (!player)
-            return;
-
-        // jump around randomly
-        if (this.groundObject && rand() < .01 && this.pos.distance(player.pos) < 20)
-        {
-            this.velocity = vec2(rand(.1,-.1), rand(.4,.2));
-            sound_jump.play(this.pos, .4, 2);
-        }
-
-        // damage player if touching
-        if (isOverlapping(this.pos, this.size, player.pos, player.size))
-            player.damage(1, this);
-    }
-
-    kill()
-    {
-        if (this.destroyed)
-            return;
-
-        ++score;
-        sound_score.play(this.pos);
-        makeDebris(this.pos, this.color);
-        this.destroy();
-    }
-       
-    render()
-    {
-        // bounce by changing size
-        const bounceTime = this.bounceTime*6;
-        this.drawSize = vec2(1-.1*Math.sin(bounceTime), 1+.1*Math.sin(bounceTime));
-
-        // make bottom flush
-        let bodyPos = this.pos;
-        bodyPos = bodyPos.add(vec2(0,(this.drawSize.y-this.size.y)/2));
-        drawTile(bodyPos, this.drawSize, this.tileInfo, this.color, this.angle, this.mirror, this.additiveColor);
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
 class Grenade extends GameObject
 {
     constructor(pos) 
